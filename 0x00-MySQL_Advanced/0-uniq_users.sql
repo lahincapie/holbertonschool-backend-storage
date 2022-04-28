28d71c853941e8b4719b4d71d05d3f9d15537a02
lahincapie <alejandra612@hotmail.com>
2022-04-28 15:28:15 -0500

    0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -5,8 +5,8 @@
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `email` VARCHAR(255) NOT NULL UNIQUE,
-    `name` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`),
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`email` varchar(255) NOT NULL UNIQUE,
+	`name` varchar(255),
+	PRIMARY KEY (`id`)
 );
