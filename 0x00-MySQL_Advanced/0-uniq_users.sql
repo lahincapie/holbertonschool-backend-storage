85d38804a80e14a39f492f814b0ececd8c5ce750
lahincapie <alejandra612@hotmail.com>
2022-04-28 12:39:35 -0500

    0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_tvshows`.`users` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(255) NOT NULL,
-    `name` VARCHAR(32) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE INDEX `email_UNIQUE`
     (`email` ASC)
