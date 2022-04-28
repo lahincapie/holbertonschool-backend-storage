db014b3b56fbdcb720f8c14ce1f26331583ef2c0
lahincapie <alejandra612@hotmail.com>
2022-04-28 15:17:27 -0500

    0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -3,8 +3,8 @@
 -- email, string (255 characters), never null and unique
 -- name, string (255 characters)
 
-DROP TABLE IF EXISTS `hbtn_0d_tvshows`.`users`;
-CREATE TABLE `hbtn_0d_tvshows`.`users` (
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(255) NOT NULL UNIQUE,
     `name` VARCHAR(255) NOT NULL,
