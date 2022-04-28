51e7dfd902f88f9023abeef542724de8126f280a
lahincapie <alejandra612@hotmail.com>
2022-04-28 12:33:01 -0500

    0-uniq_users.sql

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,14 @@
+-- creates a table users
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_tvshows`.`users` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(32) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `email_UNIQUE`
+    (`email` ASC)
+);
