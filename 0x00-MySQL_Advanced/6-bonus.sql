48077c9222747ba3c3e0584d5a5a471722d1d6e7
lahincapie <alejandra612@hotmail.com>
2022-04-28 21:35:49 -0500

    6-bonus.sql

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -8,13 +8,12 @@
 -- score, the score value for the correction
 -- Context: Write code in SQL is a nice level up!
 
-DELIMITER |
-
+DELIMITER $$
 DROP PROCEDURE IF EXISTS AddBonus;
 CREATE PROCEDURE AddBonus(
-	IN `user_id` INT,
+	IN `user_id` INTEGER,
 	IN `project_name` VARCHAR(255),
-	IN `score` INT
+	IN `score` INTEGER
 )
 BEGIN
     INSERT INTO projects (name)
@@ -23,5 +22,5 @@ BEGIN
 
     INSERT INTO corrections (user_id, project_id, score)
     VALUES(user_id, (SELECT id from projects WHERE name=project_name), score);
-END
-DELIMITER ;
+END $$
+DELIMITER ;$$
